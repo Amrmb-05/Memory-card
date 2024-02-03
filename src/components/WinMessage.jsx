f4e7668369fb1fd3bc2051057b3d0187ee14370a
amrmohamed <amoree.mb@gmail.com>
2024-02-03 14:04:49 +0200

    Create WinMessage component

---
+++ src/components/WinMessage.jsx
@@ -0,0 +1,12 @@
+import '../style/Win.css'
+
+export default function WinMessage() {
+    return (
+        <div className="win-message">
+            <p>
+                Congratulations!
+                You saved planet Namek
+            </p>
+        </div>
+    )
+}
